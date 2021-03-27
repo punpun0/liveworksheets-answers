1a22f4517a74a196eda0c5a6681e57225ed7d4cb
punpun0 <dovydas.vaicenas@gmail.com>
2021-03-27 23:13:28 +0200

    Create answersEx.js

---
+++ answersEx.js
@@ -0,0 +1,38 @@
+var data = contenidojson;
+var results = [];
+var drag = [];
+var dragResults = "";
+
+console.log('Liveworksheets.com Atsakymai\nAtsakymus per pasvirąjį brūkšnį (/) reikės atrinkti pačiam.\nPasirinkimo atsakymai (choose:A/B/C) turi žvaigždutę (*) prie teisingo atsakymo.\nDrag & drop atsakymai neveikia ir yra panaikinti! Todėl atsakymų lentelė gali eiti ne iš eilės.\npunpun @2021');
+
+for (var i = 0, len = data.length; i < len; i++) {
+	if (data[i][0].startsWith("drag")){
+    drag.push([data[i][0],data[i][2]]);
+  }
+  else if (data[i][0].startsWith("drop") && (data[i+1][0].startsWith("drop") === false)){
+    drag.sort(function(a,b){return a[1]-b[1]});
+    for (var j = 0, l = drag.length; j < l; j++){
+      dragResults += `${drag[j][0].replace("drag:"," ")}`;
+    }
+    dragResults += "\n";
+    drag = [];
+    continue;
+  }
+  else if (data[i][0].startsWith("playmp3") || data[i][0].startsWith("link") || data[i][0].startsWith("select") || data[i][0].startsWith("drop")){
+    continue;
+  }
+  else {
+    results.push([data[i][0],data[i][1]]);
+  }
+}
+results.sort(function(a,b){return a[1]-b[1]});
+
+if (dragResults.length !== 0){
+  console.log(`Drag & Drop atsakymai (experimental):\n${dragResults}`);
+}
+
+console.log("Kitų užduočių atsakymai:");
+
+for (var i = 0; i < results.length; i++){
+	console.log(`${i+1}. ${results[i][0].replaceAll("$","'")}`);
+}
