6df77af4aa17c89322ccbdce84eadb3b9d17aa0d
punpun0 <dovydas.vaicenas@gmail.com>
2021-03-28 15:37:52 +0300

    Add sorting by X and Y axis
    To prevent displacement of answers if the answer boxes are close to each other. If y axis are equal or x axis are equal. However if the answer boxes are placed inaccurately by the worksheet creator this isn't working too correctly.

--- answersEx.js
+++ answersEx.js
@@ -4,13 +4,12 @@ console.log(
 Choice answers have a star (choose:A/*B/C) next to the correct answer.
 Some answers (e.g. drag & drop) have been removed because they don't work and are removed!
 So the answer table could not be going in succession.
-
 @punpun 2021
-
 `);
 
 // entry point, all the answers are stored in contenidojson variable
-var data = contenidojson;
+var data = JSON.parse(contenidojson);
+console.log(data);
 var results = [];
 var drag = [];
 var dragResults = "";
@@ -22,7 +21,7 @@ for (var i = 0, len = data.length; i < len; i++) {
   } else if (data[i][0].startsWith("drop") && (data[i + 1][0].startsWith("drop") === false)) {
     // generate number sequence for the drag & drop answers
     drag.sort(function(a, b) {
-      return a[1] - b[1]
+      return a[1]-b[1];
     });
     for (var j = 0, l = drag.length; j < l; j++) {
       dragResults += `${drag[j][0].replace("drag:"," ")}`;
@@ -35,14 +34,22 @@ for (var i = 0, len = data.length; i < len; i++) {
   else if (data[i][0].startsWith('playmp3') || data[i][0].startsWith('join') || data[i][0].startsWith('drop') || data[i][0].startsWith('select') || data[i][0].startsWith('tick') || data[i][0].startsWith('link') || data[i][0].startsWith('speak') || data[i][0].startsWith('listen') || data[i][0].startsWith('value')) {
     continue;
   } else {
-    // grab the answer and the Y axis coordinates
-    results.push([data[i][0], data[i][1]]);
+    // grab the answer, X and Y axis coordinates
+    results.push([data[i][0], data[i][1], data[i][2]]);
   }
 }
 
 // sort by Y axis
 results.sort(function(a, b) {
-  return a[1] - b[1]
+  return a[1]-b[1];
+});
+
+// sort by X axis
+results.sort(function(a, b) {
+  if((a[1] >= b[1] && a[2] >= b[2])){
+    return 1;
+  }
+  return 0;
 });
 
 // print results
